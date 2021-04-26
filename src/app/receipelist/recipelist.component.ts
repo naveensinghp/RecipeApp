c0918810802a0efc1ce2c91583c7638f42365d8b
naveensinghp <naveensingh.mtp@gmail.com>
2021-04-26 10:57:53 +0530

    “ButtonIconAlignment”

---
+++ src/app/receipelist/recipelist.component.ts
@@ -0,0 +1,16 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipelist',
+  templateUrl: './recipelist.component.html',
+  styleUrls: ['./recipelist.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
